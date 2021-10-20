347cba3f4219efcc186ff578206e24d309a60863
Erik Nicander <toomanynamesreally@gmail.com>
2021-10-20 14:47:10 +0200

    changed theme to light

--- RenderWindow.scala
+++ RenderWindow.scala
@@ -14,12 +14,16 @@ class RenderWindow(
 
 
     //draw black without the frame
+    def clear2(x: Int, y: Int): Unit =
+        window.fill(x * blockSize, y * blockSize, blockSize, blockSize, java.awt.Color(0xff, 0xff, 0xff))
+    
     def clear(x: Int, y: Int): Unit =
-        window.fill(x * blockSize, y * blockSize, blockSize, blockSize, java.awt.Color(0x00, 0x00, 0x00))
+        window.fill(x * blockSize, y * blockSize, blockSize, blockSize, java.awt.Color(0xd0, 0xd0, 0xd0))
+        window.fill(x * blockSize + 1, y * blockSize + 1, blockSize - 2, blockSize - 2, java.awt.Color(0xff, 0xff, 0xff))
 
     def draw(x: Int, y: Int, color: java.awt.Color): Unit =
-        window.fill(x * blockSize, y * blockSize, blockSize, blockSize, java.awt.Color(0x22, 0x22, 0x22)) //frame
-        window.fill(x * blockSize + 3, y * blockSize + 3, blockSize - 6, blockSize - 6, color) //colored block
+        window.fill(x * blockSize, y * blockSize, blockSize, blockSize, java.awt.Color(0x18, 0x13, 0x1a)) //frame
+        window.fill(x * blockSize + 2, y * blockSize + 2, blockSize - 4, blockSize - 4, color) //colored block
     
     def drawFull(x: Int, y: Int, color: java.awt.Color): Unit =
         window.fill(x * blockSize, y * blockSize, blockSize, blockSize, color)
